a7e82ae9b94b5657dca983ba87362c7393549106
Fernando Ayats <ayatsfer@gmail.com>
2021-03-27 01:07:14 +0100

    Arch merge

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,8 @@
 Import-Module oh-my-posh
-Set-PoshPrompt -Theme "C:\Users\ayats\Documents\PowerShell\viper_mod.omp.json"
+Set-PoshPrompt -Theme "~\.config\oh-my-posh\viper_mod.omp.json"
 Set-PSReadLineOption -predictionsource history
-Function Function1 {Set-Location C:\Users\ayats\Documents\aurea_test1\jsbsim\ && conda activate aurea && Clear-Host}
-Function Function2 { adb forward tcp:8873 tcp:8873 && rsync -avPH --info=progress2 /cygdrive/x/Music rsync://localhost:8873/sdcard && adb forward --remove-all}
+Function Function1 {Set-Location ~\Documents\aurea_test1\jsbsim\ && conda activate aurea && Clear-Host}
 Set-Alias -Name pyaurea -Value Function1
-Set-Alias -Name rcosas -Value Function2
 $env:FLASK_ENV="development"
 Import-Module -Name Terminal-Icons
 $env:GREP_OPTIONS = '--color=always'
