e3746e932479b0ee4bedce1a56cf83d8cc0279e3
Fernando Ayats <ayatsfer@gmail.com>
2021-03-28 12:59:15 +0200

    28 Marzo

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,10 @@
 set -g -x fish_greeting ''
 oh-my-posh --init --shell fish --config ~/.config/oh-my-posh/viper_mod.omp.json | source
+export GREP_OPTIONS='--color=always'
+
+
+
+# pyenv init
+if command -v pyenv 1>/dev/null 2>&1
+  pyenv init - | source
+end
