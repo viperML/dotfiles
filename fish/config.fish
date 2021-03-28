c4a5d8d51209adc2ad43a7db692ba5d00b7048fd
Fernando Ayats <ayatsfer@gmail.com>
2021-03-28 22:41:29 +0200

    28 Marzo

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,14 @@
 set -g -x fish_greeting ''
 oh-my-posh --init --shell fish --config ~/.config/oh-my-posh/viper_mod.omp.json | source
 export GREP_OPTIONS='--color=always'
+export DOTFILES_DIR=$HOME/.dotfiles
+
+function dot_install
+  command pwsh $DOTFILES_DIR/dotbot.ps1 -c $DOTFILES_DIR/install-arch.yaml
+  command xrdb -merge $HOME/.Xresources
+end
+
+alias alas="adb devices && source $HOME/AzurLaneAutoScript/venv/bin/activate.fish && python $HOME/AzurLaneAutoScript/alas_en.pyw"
 
 
 
