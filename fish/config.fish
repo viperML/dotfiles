b7c7179d2bd63b33fe67213a6d6cc98a9b88a89a
Fernando Ayats <ayatsfer@gmail.com>
2021-07-14 08:46:07 +0200

    Wed Jul 14 08:46:07 AM CEST 2021

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ oh-my-posh --init --shell fish --config ~/.config/oh-my-posh/viper.omp.json | so
 
 
 # Variables
-export GREP_OPTIONS='--color=always'
+# export GREP_OPTIONS='--color=always'
 export VIRTUAL_ENV_DISABLE_PROMPT=true
 
 # Aliases
@@ -22,6 +22,54 @@ if status --is-interactive
     abbr --add --global ss sudo systemctl
     abbr --add --global us systemctl --user
     abbr --add --global se sudo -E systemctl edit
+
+    # Git abbreviations
+    # https://gist.github.com/james2doyle/6e8a120e31dbaa806a2f91478507314c
+    # tj git aliases
+    abbr -a gd "git diff -M"
+    abbr -a ga "git add"
+    abbr -a gaa "git add --all ."
+    abbr -a gbd "git branch -D"
+    abbr -a gs "git status"
+    abbr -a gca "git commit -a -m"
+    abbr -a gm "git merge --no-ff"
+    abbr -a gpt "git push --tags"
+    abbr -a gp "git push"
+    abbr -a grh "git reset --hard"
+    abbr -a gb "git branch"
+    abbr -a gcob "git checkout -b"
+    abbr -a gco "git checkout"
+    abbr -a gba "git branch -a"
+    abbr -a gcp "git cherry-pick"
+    abbr -a gl "git log --pretty=format:\"%Cgreen%h%Creset - %Cblue%an%Creset @ %ar : %s\""
+    abbr -a gl2 "git log --pretty='format:%Cgreen%h%Creset %an - %s' --graph"
+    abbr -a glv "git log --stat"
+    abbr -a gpom "git pull origin master"
+    abbr -a gcd 'cd "`git rev-parse --show-toplevel`"'
+    # remove files that are not under version control
+    abbr -a gcf "git clean -fd"
+    # discard changes in the working directory
+    abbr -a gcod "git checkout -- ."
+    # grab the latest upstream version
+    abbr -a gpum "git pull upstream master"
+    # delete branch from github. follow with branch name
+    abbr -a gpod "git push origin --delete"
+    # show git status without untracked files
+    abbr -a gsu "git status -uno"
+    # commit -m
+    abbr -a gcm "git commit -m"
+    abbr -a gcv "git commit --verbose"
+    abbr -a gc "git commit --verbose"
+    # diff in sublime
+    abbr -a gds "git diff | sublime"
+    # remove staged file
+    abbr -a grm "git reset HEAD"
+    # add current files, commit those file
+    abbr -a gacm "git add . --all; git commit --verbose"
+    # list the git tags by date
+    abbr -a gtd "git log --tags --simplify-by-decoration --pretty=\"format:%ai %d\""
+    # list stats for the repo
+    abbr -a grs "git shortlog -s -n --all --no-merges"
 end
 
 # Fix ssh passing wrong $TERM
