2ef40ee7373e87ad8b76616445d06edb3a4576b5
Fernando Ayats <ayatsfer@gmail.com>
2021-10-03 23:08:48 +0200

    Flake testing

--- home.nix
+++ home.nix
@@ -7,7 +7,7 @@
 
   home.packages = [
     /* pkgs.fortune */
-    nix-prefetch
+    pkgs.nix-prefetch-scripts
   ];
 
 
@@ -18,23 +18,26 @@
   
   programs.neovim = {
       enable = true;
-      plugins = with pkgs.vimPlugins; [
-        dracula-vim
-      ];
       extraConfig = ''
         ${builtins.readFile ./neovim/base.vim}
       '';
       
-#      let
-#        nvim-transparent = pkgs.vimUtils.buildVimPlugin {
-#          name = "nvim-transparent";
-#          src = pkgs.fetchFromGitHub {
-#            owner = "xiyaowong";
-#            repo = "nvim-transparent";
-#            rev = "9441bc7b03a31ccf301b984e36f0c4b4db4974a5";
-#
-#          };
-#        };
+
+      plugins = with pkgs.vimPlugins;
+        let
+          nvim-transparent = pkgs.vimUtils.buildVimPlugin {
+            name = "nvim-transparent";
+            src = pkgs.fetchFromGitHub {
+              owner = "xiyaowong";
+              repo = "nvim-transparent";
+              rev = "9441bc7b03a31ccf301b984e36f0c4b4db4974a5";
+              sha256 = "0l0fj2ifxl9p7z7cyn64a64wihl1ypddxsxdbgym5f2akjdbcqsr";
+            };
+          };
+        in [
+          nvim-transparent
+          dracula-vim
+        ];
 
 
       #   let
