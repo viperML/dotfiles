954602791b3ddb09491212ead45734193949a327
Fernando Ayats <ayatsfer@gmail.com>
2021-10-05 18:11:19 +0200

    Flake testing

--- home.nix
+++ home.nix
@@ -22,7 +22,17 @@
 
   programs.fish = {
     enable = true;
-    plugins = with pkgs.fishPlugins; [ clownfish ];
+    plugins = with pkgs.fishPlugins; [ 
+        {
+          name = "z";
+          src = pkgs.fetchFromGitHub {
+              owner = "jethrokuan";
+              repo = "z";
+              rev = "ddeb28a7b6a1f0ec6dae40c636e5ca4908ad160a";
+              sha256 = "0c5i7sdrsp0q3vbziqzdyqn4fmp235ax4mn4zslrswvn8g3fvdyh";
+          };
+        }
+     ];
   };
   
   programs.neovim = {
