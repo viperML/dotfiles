dd70e1f924be3ea5e693a5a67c54b1694de1707c
Fernando Ayats <ayatsfer@gmail.com>
2021-10-05 14:55:13 +0200

    Flake testing

--- bin/install.sh
+++ bin/install.sh
@@ -6,10 +6,13 @@ read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
     echo -n "Installing Nix..."
     curl -L https://nixos.org/nix/install | sh
-    source ~/.nix-profile/etc/profile.d/nix.sh
-    nix-channel --add https://nixos.org/channels/nixpkgs-unstable
-    nix-channel --update
-    nix-env -iA nixpkgs.nixUnstable
+    wait 1
+    NIX_PROFILES=/nix/var/nix/profiles/default "${HOME}"/.nix-profile
+    NIX_PATH="${HOME}"/.nix-defexpr/channels
+    PATH="${PATH}:${HOME}/.nix-profile/bin"
+    "${HOME}/.nix-profile/bin/nix-channel" --add https://nixos.org/channels/nixpkgs-unstable
+    "${HOME}/.nix-profile/bin/nix-channel" --update
+    "${HOME}/.nix-profile/bin/nix-env" -iA nixpkgs.nixUnstable
     mkdir -p ~/.config/nix
     echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
     echo "source ${HOME}/.nix-profile/etc/profile.d/nix.sh" >> ~/.bashrc
