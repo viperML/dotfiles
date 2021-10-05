2705da9b1ce1620d3331ee6ddfb9decbab6def62
Fernando Ayats <ayatsfer@gmail.com>
2021-10-06 00:30:21 +0200

    Starship

--- fish/fish.nix
+++ fish/fish.nix
@@ -6,7 +6,7 @@
     starship
   ];
 
-  home.file.".config/starship.toml" = ./starship.toml;
+  home.file.".config/starship.toml".source = ./starship.toml;
 
   programs.fish = {
     enable = true;
