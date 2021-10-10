bdd8e3f1a6b9ede0f515885e9e2d86ff73fc14d5
Fernando Ayats <ayatsfer@gmail.com>
2021-10-10 20:06:42 +0200

    vscode testing

--- vscode/vscode.nix
+++ vscode/vscode.nix
@@ -2,7 +2,7 @@
 { config, pkgs, ... }:
 {
   programs.vscode.enable = true;
-  programs.vscode.package = pkgs.vscode-fhs;
+  # programs.vscode.package = pkgs.vscode-fhs;
 
   home.file.".config/Code/User/keybindings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/keybindings.json";
   home.file.".config/Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/settings.json";
