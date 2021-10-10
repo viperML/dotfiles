adf06e8780ee06341eac3a5a2a400a1167bc8c90
Fernando Ayats <ayatsfer@gmail.com>
2021-10-10 19:42:28 +0200

    vscode testing

--- vscode/vscode.nix
+++ vscode/vscode.nix
@@ -6,5 +6,8 @@
 
   home.file.".config/Code/User/keybindings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/keybindings.json";
   home.file.".config/Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/settings.json";
+  home.file.".config/Code/User/snippets/bibtex.code-snippets".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/snippets/bibtex.code-snippets";
+  home.file.".config/Code/User/snippets/latex.code-snippets".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/snippets/latex.code-snippets";
+  home.file.".config/Code/User/snippets/service.code-snippets".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/snippets/service.code-snippets";
 
 }
