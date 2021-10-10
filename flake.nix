ab9bb37896fc9e0f677fdeb6ee38a88646d0b21b
Fernando Ayats <ayatsfer@gmail.com>
2021-10-10 12:40:38 +0200

    Testing nur

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
   inputs.home-manager.url = "github:nix-community/home-manager";
   # Home-manager input should match nixpkgs
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nur.url = "github:nix-community/NUR";
 
   # We take nixpkgs and home-manager as input
-  outputs = { self, nixpkgs, home-manager }: {
+  outputs = { self, nixpkgs, home-manager, nur }: {
     # With that we export homeManagerConfigurations.user
     homeManagerConfigurations = {
       ayats = home-manager.lib.homeManagerConfiguration {
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -4,6 +4,14 @@
   home.username = "ayats";
   home.homeDirectory = "/home/ayats";
 
+    nixpkgs.config.packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+  };
+
+  environment.systemPackages = [ pkgs.nur.repos.xe.comma ];
+
   # Generic programs
   home.packages = with pkgs; [
     nix-prefetch-scripts
