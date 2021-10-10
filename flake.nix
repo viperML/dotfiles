eb13b0e9ad1fb8bf4220d93de1954c79820c1234
Fernando Ayats <ayatsfer@gmail.com>
2021-10-10 13:13:01 +0200

    Testing nur

--- flake.nix
+++ flake.nix
@@ -1,39 +1,49 @@
 {
-  # We take nixpkgs unstable as input
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.home-manager.url = "github:nix-community/home-manager";
-  # Home-manager input should match nixpkgs
-  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  description = "Fernando Ayats's system configuraion";
 
-  inputs.nixpkgs.config.packagesOverrides = pkgs: {
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-      inherit pkgs;
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nur.url = "github:nix-community/NUR";
   };
 
   # We take nixpkgs and home-manager as input
-  outputs = { self, nixpkgs, home-manager }: {
-    # With that we export homeManagerConfigurations.user
-    homeManagerConfigurations = {
-      ayats = home-manager.lib.homeManagerConfiguration {
-        configuration = { pkgs, lib, ... }: {
-          imports = [
-            # Split configs per package
-            ./home-manager/home.nix
-            ./neovim/nvim.nix
-            ./fish/fish.nix
-            ./bat/bat.nix
-            ./lsd/lsd.nix
-            ./neofetch/neofetch.nix
-          ];
-          nixpkgs = {
-            config = { allowUnfree = true; };
-          };
-        };
-        system = "x86_64-linux";
-        homeDirectory = "/home/ayats";
-        username = "ayats";
+  outputs = inputs @ { self, nixpkgs, home-manager, nur }:
+    let
+
+      pkgs = import nixpkgs {
+        inherit system;
+        config = { allowBroken = true; allowUnfree = true; };
+        overlays = [
+          nur.overlay
+        ];
       };
+
+      system = "x86_64-linux";
+
+    in {
+      homeManagerConfigurations = {
+        ayats = home-manager.lib.homeManagerConfiguration {
+          configuration = { pkgs, lib, ... }: {
+            imports = [
+              # Split configs per package
+              ./home-manager/home.nix
+              ./neovim/nvim.nix
+              ./fish/fish.nix
+              ./bat/bat.nix
+              ./lsd/lsd.nix
+              ./neofetch/neofetch.nix
+            ];
+            nixpkgs = {
+              config = { allowUnfree = true; };
+            };
+          };
+          system = "x86_64-linux";
+          homeDirectory = "/home/ayats";
+          username = "ayats";
+        };
+      };
+
     };
-  };
 }
