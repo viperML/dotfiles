c3c007bb6c8b46d9f2632cc15d7eeeeba9ed3b42
Fernando Ayats <ayatsfer@gmail.com>
2021-12-10 12:16:27 +0100

    xonsh testing

--- xonsh/xonsh.nix
+++ xonsh/xonsh.nix
@@ -1,19 +1,7 @@
 { config, pkgs, ... }:
 {
-  home.packages = with pkgs;
-  let
-    xonsh-direnv = pkgs.python3Packages.buildPythonPackage {
-      name = "xonsh-direnv";
-      src = fetchFromGitHub {
-        owner = "74th";
-        repo = "xonsh-direnv";
-        rev = "85c9a378599ab560cf2d41b8a5c1f15a233aa228";
-        sha256 = "1hr43g5blyqpc9xvd3v27s48bqc8mnc0vxficqfcghbqmi5jhfvb";
-      };
-    };
-  in [
+  home.packages = with pkgs; [
     xonsh
-    xonsh-direnv
   ];
 
   home.file.".xonshrc".source = ./xonshrc;
