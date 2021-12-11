44ca2e7b05961c18334f7ff279b1650cbe058145
Fernando Ayats <ayatsfer@gmail.com>
2021-12-11 10:01:09 +0100

    Testing gui apps nix

--- nix/gui.nix
+++ nix/gui.nix
@@ -8,14 +8,10 @@
   targets.genericLinux.enable = true;
 
   home.extraProfileCommands = ''
-  if [[ -d "$out/share/applications" ]] ; then
     ${pkgs.desktop-file-utils}/bin/update-desktop-database $out/share/applications
-    echo "Updating desktop database..."
-  fi
   '';
 
   home.packages = with pkgs; [
     desktop-file-utils
-    atom
   ];
 }
