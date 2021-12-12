88efbd17f3b8e1c871023d7a73255b240a8a1cec
Fernando Ayats <ayatsfer@gmail.com>
2021-12-12 12:49:48 +0100

    Added starship to powershell

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
 Set-PSReadLineOption -predictionsource history
 $env:GREP_OPTIONS = '--color=always'
 Invoke-Expression (&starship init powershell)
+$ENV:STARSHIP_CONFIG = "C:\Users\ayats\.dotfiles\fish\starship.toml"
