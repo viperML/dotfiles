1d754195f76945685405b738fce6c72609c58058
Fernando Ayats <ayatsfer@gmail.com>
2021-12-12 12:47:12 +0100

    Added starship to powershell

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,3 @@
-Invoke-Expression (oh-my-posh --init --shell pwsh --config ~\.dotfiles\oh-my-posh\viper.omp.json)
 Set-PSReadLineOption -predictionsource history
-Function FnLSD {lsd}
-Function FnLSA {lsd -a}
-Function FnLSL {lsd -l -a}
-Function FnLST {lsd -t -r}
-Set-Alias -Name ls -Value FnLSD
-Set-Alias -Name la -Value FnLSA
-Set-Alias -Name ll -Value FnLSL
-Set-Alias -Name lt -Value FnLST
 $env:GREP_OPTIONS = '--color=always'
-#$env:DOTFILES_DIR=Join-Path -Path $env:USERPROFILE -ChildPath '.dotfiles'
+Invoke-Expression (&starship init powershell)
