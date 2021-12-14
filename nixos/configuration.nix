0285054ae8c06035a7f040e81fefb307066c4a20
Fernando Ayats <ayatsfer@gmail.com>
2021-12-14 12:32:51 +0100

    nixos: allowUnfree

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -82,7 +82,8 @@
     vscode-fhs
   ];
 
-  environment.etc."nixpkgs/config.nix".source = ../nix/config.nix;
+  # environment.etc."nixpkgs/config.nix".source = ../nix/config.nix;
+  nixpkgs.config.allowUnfree = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
