855dbbdd28599f4c7a49fb125c3e0dd3a0294a67
Fernando Ayats <ayatsfer@gmail.com>
2021-12-14 12:53:07 +0100

    using flake-utils

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -5,31 +5,7 @@
 { config, pkgs, ... }:
 
 {
-  #  imports =
-  #    [ # Include the results of the hardware scan.
-  #      ./hardware-configuration.nix
-  #    ];
-
-  # Use the systemd-boot EFI boot loader.
-  #boot.loader.systemd-boot.enable = true;
-  #boot.loader.efi.canTouchEfiVariables = true;
-
-  # networking.hostName = "nixos-vm"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Set your time zone.
   time.timeZone = "Europe/Madrid";
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  # networking.useDHCP = false;
-  # networking.interfaces.docker0.useDHCP = true;
-  # networking.interfaces.eno1.useDHCP = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
@@ -38,9 +14,6 @@
   #   keyMap = "us";
   # };
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
   services = {
     xserver = {
       enable = true;
@@ -48,8 +21,6 @@
       desktopManager.plasma5.enable = true;
     };
   };
-
-
 
   # Configure keymap in X11
   # services.xserver.layout = "us";
@@ -65,44 +36,23 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ayats = {
     isNormalUser = true;
     initialPassword = "1234";
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     brave
     latte-dock
     kitty
     libsForQt5.bismuth
     vscode-fhs
+    papirus-icon-theme
+    jetbrains-mono
   ];
 
-  # environment.etc."nixpkgs/config.nix".source = ../nix/config.nix;
   nixpkgs.config.allowUnfree = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
