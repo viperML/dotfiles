e1801ebde86d8d14a94a0cb3fb8820f4e04d3c75
Fernando Ayats <ayatsfer@gmail.com>
2021-12-14 20:15:42 +0100

    flake-utils: lock rev

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    utils.url = github:gytis-ivaskevicius/flake-utils-plus;
+    utils.url = github:gytis-ivaskevicius/flake-utils-plus/v1.3.0;
     home-manager = {
       url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
