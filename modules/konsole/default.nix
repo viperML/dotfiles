2843b80221e5a43ecac957086fb4dad8430eeae1
Fernando Ayats <ayatsfer@gmail.com>
2021-12-15 20:08:51 +0100

    modules: move konsole

--- modules/konsole/default.nix
+++ modules/konsole/default.nix
@@ -6,5 +6,6 @@
     ];
 
     file.".local/share/konsole/Dracula.colorscheme".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/modules/konsole/Dracula.colorscheme";
+    file.".local/share/konsole/Main.profile".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/modules/konsole/Main.profile";
   };
 }
