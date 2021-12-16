83c3914a837a003674f9b277305b8b10157b4066
Fernando Ayats <ayatsfer@gmail.com>
2021-12-16 23:05:43 +0100

    move vscode

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -7,9 +7,9 @@
   # Using symlinks to .dotdir instead of the nix store
   # VSCode is a pain to configure in declarative way
   # TODO - apparently we cannot reference relatives files from here
-  home.file.".config/Code/User/keybindings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/keybindings.json";
-  home.file.".config/Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/settings.json";
-  home.file.".config/Code/User/snippets/bibtex.code-snippets".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/snippets/bibtex.code-snippets";
-  home.file.".config/Code/User/snippets/latex.code-snippets".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/snippets/latex.code-snippets";
-  home.file.".config/Code/User/snippets/service.code-snippets".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/snippets/service.code-snippets";
+  home.file.".config/Code/User/keybindings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/vscode/keybindings.json";
+  home.file.".config/Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/vscode/settings.json";
+  home.file.".config/Code/User/snippets/bibtex.code-snippets".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/vscode/snippets/bibtex.code-snippets";
+  home.file.".config/Code/User/snippets/latex.code-snippets".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/vscode/snippets/latex.code-snippets";
+  home.file.".config/Code/User/snippets/service.code-snippets".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vscode/vscode/snippets/service.code-snippets";
 }
