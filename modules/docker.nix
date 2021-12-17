4c78b7084af5be4bb35b45e34502b7768e2d7985
Fernando Ayats <ayatsfer@gmail.com>
2021-12-17 11:44:37 +0100

    nixos: docker

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    storageDriver = "zfs";
+    enableOnBoot = "false";
+    enableNvidia = "true";
+  };
   users.users.ayats.extraGroups = [ "docker" ];
 }
