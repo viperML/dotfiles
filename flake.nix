532c4d7d4127a0f1f1dabc01c6ac90e09597379a
Fernando Ayats <ayatsfer@gmail.com>
2021-12-18 14:39:02 +0100

    flake refactor

--- flake.nix
+++ flake.nix
@@ -58,34 +58,30 @@
 
 
       homeConfigurations = {
-        ayats = inputs.home-manager.lib.homeManagerConfiguration {
+        genericUser = inputs.home-manager.lib.homeManagerConfiguration rec {
           system = "x86_64-linux";
-          username = "ayats";
-          homeDirectory = "/home/ayats";
+          username = "genericUser";
+          homeDirectory = "/home/genericUser";
           pkgs = self.pkgs.x86_64-linux.nixpkgs;
-          configuration = { };
-          # modules = [
-          #   self.nixosModules.neofetch
-          # ];
-          extraModules = with modules.homeModules; [
+          configuration = {
             # This home-manager module links the flake inputs into ~/.nix-inputs
             # Set the nix path into the channels from the flake
             # And then deletes the channels created with nix-channel --add ...
-            {
-              home = {
-                file = lib.mapAttrs' (name: value: { name = ".nix-inputs/${name}"; value = { source = value.outPath; }; }) inputs;
-                sessionVariables = lib.mkForce {
-                  NIX_PATH = "nixpkgs=$HOME/.nix-inputs/nixpkgs$\{NIX_PATH:+:$NIX_PATH}";
-                  FLAKE = "$HOME/.dotfiles";
-                };
-                activation.use-flake-channels = inputs.home-manager.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-                  $DRY_RUN_CMD rm -rf $VERBOSE_ARG ~/.nix-defexpr
-                  $DRY_RUN_CMD ln -s $VERBOSE_ARG /dev/null $HOME/.nix-defexpr
-                  $DRY_RUN_CMD rm -rf $VERBOSE_ARG ~/.nix-channels
-                  $DRY_RUN_CMD ln -s $VERBOSE_ARG /dev/null $HOME/.nix-channels
-                '';
+            home = {
+              file = lib.mapAttrs' (name: value: { name = ".nix-inputs/${name}"; value = { source = value.outPath; }; }) inputs;
+              sessionVariables = lib.mkForce {
+                NIX_PATH = "nixpkgs=$HOME/.nix-inputs/nixpkgs$\{NIX_PATH:+:$NIX_PATH}";
+                FLAKE = "$HOME/.dotfiles";
               };
-            }
+              activation.use-flake-channels = inputs.home-manager.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+                $DRY_RUN_CMD rm -rf $VERBOSE_ARG ~/.nix-defexpr
+                $DRY_RUN_CMD ln -s $VERBOSE_ARG /dev/null $HOME/.nix-defexpr
+                $DRY_RUN_CMD rm -rf $VERBOSE_ARG ~/.nix-channels
+                $DRY_RUN_CMD ln -s $VERBOSE_ARG /dev/null $HOME/.nix-channels
+              '';
+            };
+          };
+          extraModules = with modules.homeModules; [
             base-home
             bat
             fish
@@ -100,7 +96,13 @@
             vscode
           ];
         };
+
+        ayats = self.homeConfigurations.genericUser // {
+          username = "ayats";
+          homeDirectory = "/home/ayats";
+        };
       };
+
 
       overlay = import ./overlays;
 
