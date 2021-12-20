f472038dc9fb0311eaf1e173f228e449a3654be2
Fernando Ayats <ayatsfer@gmail.com>
2021-12-20 21:06:31 +0100

    changed nix-shell

--- shell.nix
+++ shell.nix
@@ -17,5 +17,11 @@ mkShell {
   ];
   shellHook = ''
     export FLAKE="$(pwd)"
+    echo -e "\n\e[34m❄ Welcome to viperML/dotfiles ❄"
+    echo "Last flake update:"
+    git log -1 --pretty="format:%ch" flake.lock
+    echo -e "\n\e[34m❄ Changes to the running NixOS config: ❄"
+    echo -e "\e[0m"
+    git --no-pager diff $(nixos-version --json | jq -r '.configurationRevision') -p
   '';
 }
