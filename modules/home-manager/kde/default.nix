758d6dc5ef27bc1f8a80528eb050b7438e4f24bc
Fernando Ayats <ayatsfer@gmail.com>
2021-12-26 17:20:41 +0100

    nix: fixed

--- modules/home-manager/kde/default.nix
+++ modules/home-manager/kde/default.nix
@@ -14,38 +14,48 @@
   '';
 
   home.file.".config/autostart/multiload-ng.desktop".text = ''
-  [Desktop Action Preferences]
-  Exec=${pkgs.multiload-ng}/bin/multiload-ng-systray
-  Icon=preferences-system
-  Name=Preferences
+    [Desktop Action Preferences]
+    Exec=${pkgs.multiload-ng}/bin/multiload-ng-systray
+    Icon=preferences-system
+    Name=Preferences
 
-  [Desktop Entry]
-  Actions=Preferences;
-  Categories=Utility;System;Monitor;GTK;Applet;TrayIcon;
-  Comment=Modern graphical system monitor
-  Encoding=UTF-8
-  Exec=multiload-ng-systray
-  Icon=utilities-system-monitor
-  Name=Multiload-ng (system tray)
-  StartupNotify=false
-  Terminal=false
-  TryExec=multiload-ng-systray
-  Type=Application
+    [Desktop Entry]
+    Actions=Preferences;
+    Categories=Utility;System;Monitor;GTK;Applet;TrayIcon;
+    Comment=Modern graphical system monitor
+    Encoding=UTF-8
+    Exec=multiload-ng-systray
+    Icon=utilities-system-monitor
+    Name=Multiload-ng (system tray)
+    StartupNotify=false
+    Terminal=false
+    TryExec=multiload-ng-systray
+    Type=Application
   '';
 
 
   # Latte dock config
   home.file.".config/latte/original/my-layout.layout.latte".source = ./my-layout.layout.latte;
 
+  home.file.".config/autostart/start-latte.sh.service".text = ''
+    [Desktop Entry]
+    Exec=/home/ayats/.config/autostart/start-latte.sh
+    Icon=dialog-scripts
+    Name=start-latte.sh
+    Path=
+    Type=Application
+    X-KDE-AutostartScript=true
+  '';
+
   home.file.".config/autostart/start-latte.sh" = {
     executable = true;
     text = ''
-    #!${pkgs.bash}/bin/bash
-    if [[ -f $HOME/.config/latte/my-layout.layout.latte ]]; then
-        ${pkgs.latte-dock}/bin/latte-dock --layout my-layout
-    else
-        ${pkgs.latte-dock}/bin/latte-dock --import-layout $HOME/.config/latte/original/my-layout.layout.latte
-    fi
+      #!${pkgs.bash}/bin/bash
+      if [[ -f $HOME/.config/latte/my-layout.layout.latte ]]; then
+          ${pkgs.latte-dock}/bin/latte-dock --replace --layout my-layout
+      else
+          ${pkgs.latte-dock}/bin/latte-dock --replace --import-layout $HOME/.config/latte/original/my-layout.layout.latte
+      fi
     '';
   };
 
