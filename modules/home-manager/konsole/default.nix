dd35029db6f8eca21a68e0f939d8ab8d95e1ecd1
Fernando Ayats <ayatsfer@gmail.com>
2021-12-26 12:16:46 +0100

    nix: testing konsole script

--- modules/home-manager/konsole/default.nix
+++ modules/home-manager/konsole/default.nix
@@ -8,20 +8,19 @@
     file.".local/share/konsole/Dracula.colorscheme".source = ./Dracula.colorscheme;
     file.".local/share/konsole/Main.profile".source = ./Main.profile;
 
-    activation.konsole =
-      lib.hm.dag.entryAfter [ "writeBoundary" ]
-        (inputs.self.lib.kde.configsToCommands
-          {
-            configs = {
-              konsolerc = {
-                TabBar = {
-                  TabBarPosition = "Bottom";
-                  ExpandTabWidth = false;
-                };
-              };
+    activation.konsole = lib.hm.dag.entryAfter [ "writeBoundary" ] (inputs.self.lib.kde.configsToCommands
+      {
+        configs = {
+          konsolerc = {
+            "Desktop Entry".DefaultProfile = "Main.profile";
+            MainWindow = {
+              StatusBar = "Disabled";
+              RestorePositionForNextInstance = false;
             };
-          }
-        );
+          };
+        };
+      }
+    );
 
   };
 
