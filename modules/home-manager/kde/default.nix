05659dc07d4a1ae71615f627d7fe7120db238a67
Fernando Ayats <ayatsfer@gmail.com>
2021-12-27 13:43:57 +0100

    nixos: no multiload

--- modules/home-manager/kde/default.nix
+++ modules/home-manager/kde/default.nix
@@ -3,35 +3,7 @@
 {
   home.packages = with pkgs; [
     latte-dock
-    multiload-ng
   ];
-
-  # Multiload-ng config
-  home.file.".config/multiload-ng/my-systray.conf".source = ../../misc/multiload-ng/systray.conf;
-
-  home.activation.multiload-ng = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    $DRY_RUN_CMD cp $VERBOSE_ARG ~/.config/multiload-ng/my-systray.conf ~/.config/multiload-ng/systray.conf
-  '';
-
-  home.file.".config/autostart/multiload-ng.desktop".text = ''
-    [Desktop Action Preferences]
-    Exec=${pkgs.multiload-ng}/bin/multiload-ng-systray
-    Icon=preferences-system
-    Name=Preferences
-
-    [Desktop Entry]
-    Actions=Preferences;
-    Categories=Utility;System;Monitor;GTK;Applet;TrayIcon;
-    Comment=Modern graphical system monitor
-    Encoding=UTF-8
-    Exec=multiload-ng-systray
-    Icon=utilities-system-monitor
-    Name=Multiload-ng (system tray)
-    StartupNotify=false
-    Terminal=false
-    TryExec=multiload-ng-systray
-    Type=Application
-  '';
 
 
   # Latte dock config
