a79a7672812eda21755619c9c04006f2f8646958
Fernando Ayats <ayatsfer@gmail.com>
2021-12-28 12:55:37 +0100

    gen6: secrets volume

--- modules/nixos/sops.nix
+++ modules/nixos/sops.nix
@@ -3,8 +3,8 @@
 {
   environment.systemPackages = with pkgs; [ age sops ];
 
-  sops.defaultSopsFile = ../../secrets/test.yaml;
-  sops.age.keyFile = "/home/ayats/.config/sops/age/keys.txt";
+  # sops.defaultSopsFile = ../../secrets/test.yaml;
+  sops.age.keyFile = "/secrets/age/keys.txt";
 
   sops.secrets.msaClientID.sopsFile = ../../secrets/multimc.yaml;
 
