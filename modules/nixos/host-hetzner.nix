b96cf8112639b5bfcd7ff62483b4329b7ab41ee4
Fernando Ayats <ayatsfer@gmail.com>
2022-01-02 00:46:21 +0100

    mc server: memory tweaks

--- modules/nixos/host-hetzner.nix
+++ modules/nixos/host-hetzner.nix
@@ -51,17 +51,17 @@
       fsType = "ext4";
     };
 
-  systemd.services = {
-    create-swapfile = {
-      serviceConfig.Type = "oneshot";
-      wantedBy = [ "swap-swapfile.swap" ];
-      script = ''
-        ${pkgs.coreutils}/bin/truncate -s 0 /swap/swapfile
-        ${pkgs.e2fsprogs}/bin/chattr +C /swap/swapfile
-        ${pkgs.btrfs-progs}/bin/btrfs property set /swap/swapfile compression none
-      '';
-    };
-  };
+  # systemd.services = {
+  #   create-swapfile = {
+  #     serviceConfig.Type = "oneshot";
+  #     wantedBy = [ "swap-swapfile.swap" ];
+  #     script = ''
+  #       ${pkgs.coreutils}/bin/truncate -s 0 /swap/swapfile
+  #       ${pkgs.e2fsprogs}/bin/chattr +C /swap/swapfile
+  #       ${pkgs.btrfs-progs}/bin/btrfs property set /swap/swapfile compression none
+  #     '';
+  #   };
+  # };
 
   fileSystems."/swap" = {
     device = "/dev/disk/by-label/LINUXROOT";
