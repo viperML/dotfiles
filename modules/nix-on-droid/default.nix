c696ccbbb27808fabc87edbdc689ca5b6917fc66
Fernando Ayats <ayatsfer@gmail.com>
2022-01-15 19:48:02 +0100

    nod: testing

--- modules/nix-on-droid/default.nix
+++ modules/nix-on-droid/default.nix
@@ -1,9 +1,13 @@
-{ config, pkgs, ...}:
+{ config, pkgs, inputs, ... }:
 
 {
   system.stateVersion = "21.11";
 
-  home-manager.config = { config, pkgs }: {
-    home.stateVersion = "21.11";
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+
+    config = { config, pkgs, ... }: {
+      home.stateVersion = "21.11";
+    };
   };
 }
