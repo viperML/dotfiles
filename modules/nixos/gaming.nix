9d5f61ac219c16f3827334afd341eb5e542987af
Fernando Ayats <ayatsfer@gmail.com>
2022-01-26 00:55:00 +0100

    flake: update

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -7,7 +7,7 @@
 
   environment.systemPackages = with pkgs; [
     lutris
-    (multimc.override {
+    (polymc.override {
       # Thanks https://github.com/linyinfeng/dotfiles/blob/0d86a257f34b51742f3012fb27b5ed8c173d9fe9/overlays/multimc.nix
       # for setting your API ID public
       msaClientID = "6998db6c-94fb-48bd-8d97-1e96707f1a5c";
