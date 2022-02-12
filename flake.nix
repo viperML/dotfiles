e295a62b7100ac4b85866aa750ab328998fd47fe
Fernando Ayats <ayatsfer@gmail.com>
2022-02-12 10:29:51 +0100

    no pre-commit nix

--- flake.nix
+++ flake.nix
@@ -60,10 +60,6 @@
             {
               devShell = import ./bin/devShell.nix { inherit pkgs; };
               packages = flake-utils-plus.lib.exportPackages self.overlays channels;
-              checks.pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
-                src = ./.;
-                hooks = import ./misc/hooks.nix { inherit inputs system; };
-              };
             };
       };
 
@@ -123,11 +119,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flakeUtils.follows = "flake-utils";
       inputs.flakeCompat.follows = "flake-compat";
-    };
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 }
