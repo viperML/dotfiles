be5cbd49469ecd17f979fe4ebb6f9a3dad5177f0
Fernando Ayats <ayatsfer@gmail.com>
2022-02-22 23:30:03 +0100

    statix check

--- default.nix
+++ default.nix
@@ -5,19 +5,17 @@
   crossSystem ? localSystem,
   ...
 } @ args: let
-  flake-compat = (
-    import
-    (
-      let
-        lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-      in
-        fetchTarball {
-          url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-          sha256 = lock.nodes.flake-compat.locked.narHash;
-        }
-    )
-    {src = ./.;}
-  );
+  flake-compat = import
+  (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+      fetchTarball {
+        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+        sha256 = lock.nodes.flake-compat.locked.narHash;
+      }
+  )
+  {src = ./.;};
   pkgs = flake-compat.defaultNix.legacyPackages.${system};
 in
   # If `localSystem` was explicitly passed, legacy `system` should
