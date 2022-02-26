d35549960c389ec04219f51126cbb68c17cfc24e
Fernando Ayats <ayatsfer@gmail.com>
2022-02-26 11:40:20 +0100

    restored lightlysahders

--- overlays/pkgs/lightlyshaders/default.nix
+++ overlays/pkgs/lightlyshaders/default.nix
@@ -12,13 +12,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "lightlyshaders";
-  version = "unstable-2022-02-24";
+  version = "unstable-2022-02-26";
 
   src = fetchFromGitHub {
     repo = "LightlyShaders";
-    owner = "a-parhom";
-    rev = "7e06ae57ad66a22cfac8cdbf34038050c2257194";
-    sha256 = "1m5jwxjg4l6r4rrfrlkjvsvjxjdaj2n8s86rbp6h4qcgy5bad8l2";
+    owner = "viperML";
+    rev = "e5fa193821e8eafbba4cbdceebe276cb9a95e715";
+    sha256 = "0mkallhppw6iw02hfn916r4llxsdd54p8wlk6i4llxjcdwiimqlc";
   };
 
   postConfigure = ''
