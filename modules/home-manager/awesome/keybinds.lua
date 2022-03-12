1eccff22c1f4eb90f8b4858956f8cc8874adc8df
Fernando Ayats <ayatsfer@gmail.com>
2022-03-12 11:33:19 +0100

    stylua

--- modules/home-manager/awesome/keybinds.lua
+++ modules/home-manager/awesome/keybinds.lua
@@ -1,16 +1,16 @@
 local modkey = "Mod4"
-local awful = require("awful")
+local awful = require "awful"
 
 -- Programs
-awful.keyboard.append_global_keybindings({
-	awful.key({ modkey }, "e", function()
-		awful.spawn("nautilus")
-	end),
+awful.keyboard.append_global_keybindings {
+    awful.key({ modkey }, "e", function()
+        awful.spawn "nautilus"
+    end),
 
-	awful.key({ modkey, "Shift" }, "r", function()
-		awesome.restart()
-	end, { description = "reload awesome", group = "awesome" }),
-})
+    awful.key({ modkey, "Shift" }, "r", function()
+        awesome.restart()
+    end, { description = "reload awesome", group = "awesome" }),
+}
 
 -- awful.keyboard.append_global_keybindings({
 -- 	awful.key({ modkey }, "h", hotkeys_popup.show_help, { description = "show help", group = "awesome" }),
