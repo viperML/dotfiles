e8b891741cfdfb6e695f82f7ea5f96eff37d3184
Fernando Ayats <ayatsfer@gmail.com>
2022-04-21 10:28:11 +0200

    tweak systemd-boot script

--- lib/mkSystem/systemd-boot.nix
+++ lib/mkSystem/systemd-boot.nix
@@ -9,17 +9,16 @@ with lib; let
   cfg = config.viper;
   loader-conf = "${config.boot.loader.efi.efiSysMountPoint}/loader/loader.conf";
   oldConfig = import "${modulesPath}/system/boot/loader/systemd-boot/systemd-boot.nix" {inherit pkgs config lib;};
-  # TODO check if default spec exists
   fixedBuilder = pkgs.writeShellScript "systemd-boot" ''
     echo ">>> Installing bootloader for $@"
     ${oldConfig.config.content.system.build.installBootLoader} "$@"
-    echo ">>>"
-    ${pkgs.coreutils}/bin/cat ${loader-conf}
-    echo ">>>"
-    ${pkgs.gnused}/bin/sed -i -r "s/^(default nixos-generation-[0-9]+).*/\1-specialisation-${config.viper.defaultSpec}.conf/g" ${loader-conf}
-    echo "<<<"
-    ${pkgs.coreutils}/bin/cat ${loader-conf}
-    echo "<<<"
+    generation=$(${pkgs.gnused}/bin/sed -n -r "s/default nixos-generation-([0-9]+).*/\1/p" ${loader-conf})
+    if [[ -f ${config.boot.loader.efi.efiSysMountPoint}/loader/entries/nixos-generation-$generation-specialisation-${config.viper.defaultSpec}.conf ]]; then
+      echo ">>> Changing to the selected default spec to ${config.viper.defaultSpec}"
+      ${pkgs.gnused}/bin/sed -i -r "s/^(default nixos-generation-[0-9]+).*/\1-specialisation-${config.viper.defaultSpec}.conf/g" ${loader-conf}
+    else
+      echo ">>> WARNING: the default spec was not found"
+    fi
   '';
 in {
   options.viper = {
