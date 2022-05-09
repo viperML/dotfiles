928dc588e36923e56666439b345e40a8c191348f
Fernando Ayats <ayatsfer@gmail.com>
2022-05-09 11:56:07 +0200

    testing systemd-initrd

--- hosts/gen6/init.nix
+++ hosts/gen6/init.nix
@@ -1,15 +1,6 @@
 {pkgs, ...}: {
   boot.initrd.systemd = {
     enable = true;
-    initrdBin = with pkgs; [
-      (symlinkJoin {
-        inherit (bash) name meta;
-        paths = [bash];
-        nativeBuildInputs = [makeWrapper];
-        postBuild = ''
-          ln -s $out/bin/bash $out/bin/sh
-        '';
-      })
-    ];
+    emergencyAccess = true;
   };
 }
