9b7192c1bdfbd72726a51f31099b3da1654d13e5
Fernando Ayats <ayatsfer@gmail.com>
2022-05-20 17:53:45 +0200

    HM hooks

--- homes/ayats@viperSL4/home.nix
+++ homes/ayats@viperSL4/home.nix
@@ -3,19 +3,21 @@
   pkgs,
   lib,
   self,
+  config,
   ...
 }: let
   env = {
     FLAKE = "/mnt/c/Users/ayats/Documents/dotfiles";
     # BROWSER = "${prefix}/wslu";
-    # EDITOR = "${prefix}/nvim";
-    # SHELL = "${prefix}/fish";
-    SSH_AUTH_SOCK = "/run/user/1000/ssh-agent";
+    EDITOR = "nvim";
+    SHELL = "fish";
   };
 in {
   home.sessionVariables = env;
 
   home.packages = [
+    packages.home-manager.default
+    pkgs.keychain
     packages.self.neofetch
     packages.self.vshell
     packages.self.neovim
@@ -24,5 +26,12 @@ in {
   xdg.configFile."nix/nix.conf".text = lib.mkAfter ''
     auto-optimise-store = true
     builders = ssh://ayats@gen6.ayatsfer.gmail.com.beta.tailscale.net
+  '';
+
+  xdg.configFile."nix/rc".text = ''
+    eval "$(${lib.getExe pkgs.direnv} hook bash)"
+    ${lib.getExe pkgs.keychain} -q --nogui
+    . $HOME/.keychain/$(hostname)-sh
+    ${lib.concatStringsSep "\n" (__attrValues (__mapAttrs (n: v: "export ${n}=${v}") config.home.sessionVariables))}
   '';
 }
