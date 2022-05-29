2c87187b03574dff11edc87f6547d09e490f0533
viperML <viperML@users.noreply.github.com>
2022-05-29 20:44:04 +0000

    Automatic update for packages/overrides/awesome/update.sh

--- packages/overrides/awesome/default.nix
+++ packages/overrides/awesome/default.nix
@@ -4,11 +4,11 @@
 }:
 awesome.overrideAttrs (_: {
   __nocachix = true;
-  version = "unstable-2022-03-21";
+  version = "unstable-2022-05-29";
   src = fetchFromGitHub {
     owner = "awesomeWM";
     repo = "awesome";
-    rev = "c539e0e4350a42f813952fc28dd8490f42d934b3";
-    sha256 = "111sgx9sx4wira7k0fqpdh76s9la3i8h40wgaii605ybv7n0nc0h";
+    rev = "3a542219f3bf129546ae79eb20e384ea28fa9798";
+    sha256 = "1qyy650rwxaakw4hmnvwv7lqxjz22xhbzq8vqlv6ry5g5gmg0gg3";
   };
 })
