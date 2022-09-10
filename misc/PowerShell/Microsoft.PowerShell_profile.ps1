a3f3623b0e8320199489f7f67f27e7596301033e
Fernando Ayats <ayatsfer@gmail.com>
2022-09-10 23:47:32 +0200

    use powershell

--- misc/PowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,4 +0,0 @@
-Set-PSReadLineOption -PredictionSource History
-$Env:DOTFILES = "C:\Users\ayats\Documents\dotfiles"
-$Env:STARSHIP_CONFIG = "$Env:DOTFILES\packages\overrides\vshell\starship.toml"
-Invoke-Expression (&starship init powershell)
---
+++ misc/profile.ps1
@@ -0,0 +1,22 @@
+Import-Module PSReadLine
+
+Set-PSReadlineOption -BellStyle None
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
+Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
+
+Set-PSReadLineKeyHandler -Key Alt+d -Function ShellKillWord
+Set-PSReadLineKeyHandler -Key Alt+Backspace -Function ShellBackwardKillWord
+Set-PSReadLineKeyHandler -Key Alt+b -Function ShellBackwardWord
+Set-PSReadLineKeyHandler -Key Alt+f -Function ShellForwardWord
+Set-PSReadLineKeyHandler -Key Alt+B -Function SelectShellBackwardWord
+Set-PSReadLineKeyHandler -Key Alt+F -Function SelectShellForwardWord
+
+$ESC = [char]27
+
+function prompt {
+    Write-Host "$ESC[96m$pwd$ESC[0m" -NoNewline
+    Write-Host
+
+    return "> "
+}
