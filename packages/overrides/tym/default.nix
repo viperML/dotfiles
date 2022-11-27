99c872bd969dafac0715cd232a739910b5df2f2f
Fernando Ayats <ayatsfer@gmail.com>
2022-11-27 11:19:19 +0100

    tym sixel

--- packages/overrides/tym/default.nix
+++ packages/overrides/tym/default.nix
@@ -1,20 +1,48 @@
 {
+  sources,
+  #
   tym,
   symlinkJoin,
   makeWrapper,
-}:
-symlinkJoin {
-  __nocachix = true;
-  inherit (tym) name pname version;
-  paths = [tym];
-  nativeBuildInputs = [makeWrapper];
-  postBuild = ''
-    TYM_DIR="$out/share/tym"
-    mkdir -pv "$TYM_DIR"
+  vte,
+}: let
+  myVte = vte.overrideAttrs (old: {
+    version = sources.vte.date;
+    inherit (sources.vte) src;
+    mesonFlags =
+      old.mesonFlags
+      ++ [
+        "-Dsixel=true"
+        "-Db_lto=false"
+      ];
+  });
 
-    cp -vf ${./config.lua} "$TYM_DIR/config.lua"
+  myTym =
+    (tym.override {
+      vte = myVte;
+    })
+    .overrideAttrs (old: {
+      version = sources.tym.date;
+      inherit (sources.tym) src;
+      patches =
+        (old.patches or [])
+        ++ [
+          ./sixel.patch
+        ];
+    });
+in
+  symlinkJoin {
+    __nocachix = true;
+    inherit (myTym) name pname version;
+    paths = [myTym];
+    nativeBuildInputs = [makeWrapper];
+    postBuild = ''
+      TYM_DIR="$out/share/tym"
+      mkdir -pv "$TYM_DIR"
 
-    wrapProgram $out/bin/tym \
-      --add-flags "--use=$TYM_DIR/config.lua"
-  '';
-}
+      cp -vf ${./config.lua} "$TYM_DIR/config.lua"
+
+      wrapProgram $out/bin/tym \
+        --add-flags "--use=$TYM_DIR/config.lua"
+    '';
+  }
