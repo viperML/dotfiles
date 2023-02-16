e46eeb2777be8631c4354215b4cb785747caba70
Fernando Ayats <ayatsfer@gmail.com>
2023-02-16 12:26:04 +0100

    wezterm: temp disable checks

--- packages/overrides/wezterm/default.nix
+++ packages/overrides/wezterm/default.nix
@@ -2,14 +2,18 @@
   wezterm,
   symlinkJoin,
   makeBinaryWrapper,
-}:
-symlinkJoin {
-  inherit (wezterm) name pname version;
-  paths = [wezterm];
-  nativeBuildInputs = [makeBinaryWrapper];
-  __nocachix = true;
-  postBuild = ''
-    wrapProgram $out/bin/wezterm \
-      --set-default WEZTERM_CONFIG_FILE ${./wezterm.lua}
-  '';
-}
+}: let
+  myWezterm = wezterm.overrideAttrs (old: {
+    doCheck = builtins.trace "https://github.com/NixOS/nixpkgs/issues/216394" false;
+  });
+in
+  symlinkJoin {
+    inherit (myWezterm) name pname version;
+    paths = [myWezterm];
+    nativeBuildInputs = [makeBinaryWrapper];
+    __nocachix = true;
+    postBuild = ''
+      wrapProgram $out/bin/wezterm \
+        --set-default WEZTERM_CONFIG_FILE ${./wezterm.lua}
+    '';
+  }
