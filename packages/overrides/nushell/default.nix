a33410dfad546b1096745d0546c413c6a1e5cf8c
Fernando Ayats <ayatsfer@gmail.com>
2023-02-25 15:26:32 +0100

    update nushell

--- packages/overrides/nushell/default.nix
+++ packages/overrides/nushell/default.nix
@@ -1,21 +1,60 @@
+# {
+#   nushell,
+#   symlinkJoin,
+#   makeWrapper,
+#   carapace,
+# }: let
+#   muNushell = nushell;
+# in
+# symlinkJoin {
+#   __nocachix = true;
+#   inherit (myU) name pname version meta;
+#   paths = [
+#     nushell
+#     carapace
+#   ];
+#   nativeBuildInputs = [makeWrapper];
+#   postBuild = ''
+#     wrapProgram $out/bin/nu \
+#       --add-flags "--env-config ${./env.nu}" \
+#       --add-flags "--config ${./config.nu}" \
+#       --prefix PATH ':' $out/bin
+#   '';
+# }
 {
   nushell,
+  pname,
+  version,
+  src,
+  date,
+  rustPlatform,
+  pkg-config,
+  openssl,
   symlinkJoin,
   makeWrapper,
   carapace,
-}:
-symlinkJoin {
-  __nocachix = true;
-  inherit (nushell) name pname version meta;
-  paths = [
-    nushell
-    carapace
-  ];
-  nativeBuildInputs = [makeWrapper];
-  postBuild = ''
-    wrapProgram $out/bin/nu \
-      --add-flags "--env-config ${./env.nu}" \
-      --add-flags "--config ${./config.nu}" \
-      --prefix PATH ':' $out/bin
-  '';
-}
+}: let
+  package = rustPlatform.buildRustPackage {
+    inherit pname src;
+    version = date;
+    cargoLock.lockFile = ./nushell-${version}/Cargo.lock;
+    nativeBuildInputs = [pkg-config];
+    buildInputs = [openssl];
+    doCheck = false;
+  };
+in
+  symlinkJoin {
+    __nocachix = true;
+    inherit (package) name pname version meta;
+    paths = [
+      package
+      carapace
+    ];
+    nativeBuildInputs = [makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/nu \
+        --add-flags "--env-config ${./env.nu}" \
+        --add-flags "--config ${./config.nu}" \
+        --prefix PATH ':' $out/bin
+    '';
+  }
