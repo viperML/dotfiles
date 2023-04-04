fc425e6fb57ee68f3edae1a731c63bc49c2fbd90
Fernando Ayats <ayatsfer@gmail.com>
2023-04-04 19:01:14 +0000

    add file

--- homes/ayats/home.nix
+++ homes/ayats/home.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: {
   home = {
@@ -11,6 +12,9 @@
       NOMAD_ADDR = "http://chandra.ayatsfer.gmail.com.beta.tailscale.net:4646";
     };
     stateVersion = lib.mkDefault "21.11";
+    packages = with pkgs; [
+      file
+    ];
   };
 
   unsafeFlakePath = lib.mkDefault "${config.home.homeDirectory}/Documents/dotfiles";
