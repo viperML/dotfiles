b7c15cacd1dba39d1f64ef093f48d9dca60b4a39
GitHub <noreply@github.com>
2023-05-01 04:13:44 +0000

    Cleanup
    Co-authored-by: viperML <viperML@users.noreply.github.com>

--- modules/nixos/desktop-awesome.nix
+++ modules/nixos/desktop-awesome.nix
@@ -41,14 +41,14 @@
         dolphin
         ark
         # Everything I tried to get thumbnails working
-
+        
         dolphin-plugins
         ffmpegthumbs
         kdegraphics-thumbnailers
         kio
         kio-extras
         #
-
+        
         gwenview
         kwallet
         kwalletmanager
