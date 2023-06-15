c8afe2042d9011c80557e31e3cfb2f9029b0feef
GitHub <noreply@github.com>
2023-06-15 04:20:04 +0000

    Bump packages/main/xdg-ninja
    Co-authored-by: viperML <viperML@users.noreply.github.com>

--- packages/main/xdg-ninja/generated.nix
+++ packages/main/xdg-ninja/generated.nix
@@ -4,13 +4,13 @@
   xdg-ninja = {
     pname = "xdg-ninja";
     version = "33b6491dd4561f134a89a22db5513df27219a15e";
-    src = fetchFromGitHub ({
+    src = fetchFromGitHub {
       owner = "b3nj5m1n";
       repo = "xdg-ninja";
       rev = "33b6491dd4561f134a89a22db5513df27219a15e";
       fetchSubmodules = false;
       sha256 = "sha256-9DTBEORVMlhGYpwmLUQNkksnW1eQUJW5crNLPJwn3M4=";
-    });
+    };
     date = "2023-05-21";
   };
 }
