6ab7e2ee32a9476ffb63dfa416c905d6908f6ab0
GitHub <noreply@github.com>
2023-06-15 04:10:39 +0000

    Cleanup
    Co-authored-by: viperML <viperML@users.noreply.github.com>

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  packages,
-  ...
-}: {
+{...}: {
   services.tailscale.enable = true;
   networking.firewall.interfaces.tailscale0.allowedTCPPorts = [22];
   networking.firewall.interfaces.tailscale0.allowedTCPPortRanges = [
