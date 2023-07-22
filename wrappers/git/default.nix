dc0485c8d16236ee5b972552d6aa7e975ae055a7
Fernando Ayats <ayatsfer@gmail.com>
2023-07-22 11:12:08 +0200

    tweak git

--- wrappers/git/default.nix
+++ wrappers/git/default.nix
@@ -5,7 +5,7 @@
 }: {
   wrappers.git = {
     basePackage = pkgs.git;
-    env.GIT_CONFIG_GLOBAL = pkgs.writeText "gitconfig" (lib.fileContents ./gitconfig);
+    env.GIT_CONFIG_GLOBAL = ./gitconfig;
     extraPackages = [
       pkgs.git-extras
       pkgs.delta
