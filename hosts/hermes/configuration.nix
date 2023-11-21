6a9f5d82e2be65bbf5b220723a476d16e4dee664
Fernando Ayats <ayatsfer@gmail.com>
2023-11-21 13:27:45 +0100

    consul mesh

--- hosts/hermes/configuration.nix
+++ hosts/hermes/configuration.nix
@@ -49,6 +49,12 @@
 
   services.resolved = {
     enable = true;
+    extraConfig = ''
+      [Resolve]
+      DNS=127.0.0.1:8600
+      DNSSEC=false
+      Domains=~consul
+    '';
   };
 
   environment.systemPackages = [
@@ -133,4 +139,38 @@
   };
 
   programs.darling.enable = true;
+
+  services.consul = {
+    enable = true;
+    webUi = true;
+    interface = {
+      bind = config.services.tailscale.interfaceName;
+      advertise = config.services.tailscale.interfaceName;
+    };
+    extraConfig = {
+      # server = true;
+      # bootstrap_expect = 2;
+      client_addr = ''{{ GetInterfaceIP "${config.services.tailscale.interfaceName}" }} {{ GetAllInterfaces | include "flags" "loopback" | join "address" " " }}'';
+    };
+  };
+
+  networking.firewall.interfaces.${config.services.tailscale.interfaceName} = rec {
+    allowedTCPPorts = [
+      8500
+      8600
+      8501
+      8502
+      8503
+      8301
+      8302
+      8300
+    ];
+    allowedUDPPorts = allowedTCPPorts;
+    allowedTCPPortRanges = [
+      {
+        from = 21000;
+        to = 21255;
+      }
+    ];
+  };
 }
