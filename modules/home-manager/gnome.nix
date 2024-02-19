5ebdfef9482445729fe329be6969c3f8248a9076
Fernando Ayats <ayatsfer@gmail.com>
2024-02-19 16:10:28 +0100

    overlay key

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -41,6 +41,7 @@ in
       "org/gnome/mutter" = {
         "experimental-features" = gvariant.mkArray gvariant.type.string [ "scale-monitor-framebuffer" ];
         "focus-change-on-pointer-rest" = gvariant.mkBoolean false;
+        "overlay-key" = gvariant.mkString "Super_R";
       };
       "org/gnome/settings-daemon/plugins/media-keys" = {
         "custom-keybindings" = gvariant.mkArray gvariant.type.string (map (key: "/${key}/") (builtins.attrNames shortcuts));
