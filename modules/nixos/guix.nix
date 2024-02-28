df342bc8e6a7dc0f11e75a1838cbcd80c989f8e4
Fernando Ayats <fernando.ayats-llamas@inria.fr>
2024-02-28 17:12:37 +0100

    fix ssl?

--- modules/nixos/guix.nix
+++ modules/nixos/guix.nix
@@ -1,4 +1,4 @@
-{config, ...}: {
+{
   services.guix = {
     enable = true;
     gc = {
@@ -13,4 +13,10 @@
     export GUILE_LOAD_COMPILED_PATH="$GUILE_LOAD_COMPILED_PATH:$HOME/.config/guix/current/share/guile/site/3.0"
     export GUILE_LOAD_COMPILED_PATH="$GUILE_LOAD_COMPILED_PATH:$HOME/.config/guix/current/lib/guile/3.0/site-ccache"
   '';
+
+  # guix apps can't find these
+  environment.sessionVariables = {
+    SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
+    SSL_CERT_DIR = "/etc/ssl/certs";
+  };
 }
