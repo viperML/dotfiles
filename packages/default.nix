4baeef16c072925bfc5b4f5bc157ac849ef8625c
Fernando Ayats <fernando.ayats-llamas@inria.fr>
2024-03-27 11:04:44 +0100

    add stage3

--- packages/default.nix
+++ packages/default.nix
@@ -7,46 +7,51 @@
 {
   flake.pkgsFor =
     pkgs:
-    let
-      stage1 = (
-        lib.fix (
-          self:
-          let
-            inherit (pkgs) system;
-            callPackage = lib.callPackageWith (pkgs // self);
+    lib.fix (
+      self:
+      let
+        stage1 = (
+          lib.fix (
+            self':
+            let
+              inherit (pkgs) system;
+              callPackage = lib.callPackageWith (pkgs // self');
 
-            auto = lib.pipe (builtins.readDir ./.) [
+              auto = lib.pipe (builtins.readDir ./.) [
+                (lib.filterAttrs (name: value: value == "directory"))
+                (builtins.mapAttrs (name: _: callPackage ./${name} { }))
+              ];
+            in
+            auto
+            // {
+              nix-index = callPackage ./nix-index {
+                database = inputs.nix-index-database.legacyPackages.${system}.database;
+                databaseDate = config.flake.lib.mkDate inputs.nix-index-database.lastModifiedDate;
+              };
+              fish = callPackage ./fish { inherit (pkgs) fish; };
+            }
+          )
+        );
+
+        stage2 =
+          stage1
+          // (inputs.wrapper-manager.lib {
+            pkgs = pkgs // stage1;
+            modules = lib.pipe (builtins.readDir ../modules/wrapper-manager) [
               (lib.filterAttrs (name: value: value == "directory"))
-              (builtins.mapAttrs (name: _: callPackage ./${name} { }))
+              builtins.attrNames
+              (map (n: ../modules/wrapper-manager/${n}))
             ];
+          }).config.build.packages;
+
+        stage3 =
+          let
+            callPackage = lib.callPackageWith (pkgs // self);
           in
-          auto
-          // {
-            nix-index = callPackage ./nix-index {
-              database = inputs.nix-index-database.legacyPackages.${system}.database;
-              databaseDate = config.flake.lib.mkDate inputs.nix-index-database.lastModifiedDate;
-            };
-            fish = callPackage ./fish { inherit (pkgs) fish; };
-          }
-        )
-      );
-      stage2 =
-        stage1
-        // (inputs.wrapper-manager.lib {
-          pkgs = pkgs // stage1;
-          modules = lib.pipe (builtins.readDir ../modules/wrapper-manager) [
-            (lib.filterAttrs (name: value: value == "directory"))
-            builtins.attrNames
-            (map (n: ../modules/wrapper-manager/${n}))
-          ];
-        }).config.build.packages;
-      stage3 =
-        let
-          callPackage = lib.callPackageWith (pkgs // stage2);
-        in
-        stage2 // { env = callPackage ./env { }; };
-    in
-    stage3;
+          stage2 // { env = callPackage ./env { }; };
+      in
+      stage3
+    );
 
   perSystem =
     { pkgs, system, ... }:
