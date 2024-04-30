c8b4d49c0932d838492c372ad5b5a8a7888c2b35
Fernando Ayats <ayatsfer@gmail.com>
2024-04-30 15:57:38 +0200

    no unstable-small

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } { imports = [ ./flake ]; };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
