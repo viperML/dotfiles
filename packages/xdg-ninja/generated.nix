a823cdb08327b3b3cd9925126478d93586af93d0
GitHub <noreply@github.com>
2024-07-01 04:19:35 +0000

    Bump packages/xdg-ninja/nvfetcher.toml
    Co-authored-by: viperML <viperML@users.noreply.github.com>

--- packages/xdg-ninja/generated.nix
+++ packages/xdg-ninja/generated.nix
@@ -3,14 +3,14 @@
 {
   xdg-ninja = {
     pname = "xdg-ninja";
-    version = "d56175dfbbb25541228c2d62e6b466a2ee60639b";
+    version = "66025879cd8d797841af326ec7a1c5aa496efff0";
     src = fetchFromGitHub {
       owner = "b3nj5m1n";
       repo = "xdg-ninja";
-      rev = "d56175dfbbb25541228c2d62e6b466a2ee60639b";
+      rev = "66025879cd8d797841af326ec7a1c5aa496efff0";
       fetchSubmodules = false;
-      sha256 = "sha256-BtVqbKTyM/52e1nkZNLcXaP4kDiFP/uDTLVugDDYJv8=";
+      sha256 = "sha256-8WfIZQnL6gBmsPD3xZg7VXlkBhrVcBEWoTkJpgENh68=";
     };
-    date = "2024-06-05";
+    date = "2024-06-28";
   };
 }
