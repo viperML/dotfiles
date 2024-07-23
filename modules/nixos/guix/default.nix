6ef6fb61587319d0974bac7d7ce4b90fcd14f09a
Fernando Ayats <fernando.ayats-llamas@inria.fr>
2024-07-23 15:32:41 +0200

    tweak guix-daemon

--- modules/nixos/guix/default.nix
+++ modules/nixos/guix/default.nix
@@ -11,27 +11,27 @@
       enable = true;
       dates = "weekly";
     };
-    extraArgs =
-      let
-        substituters = [
-          "https://ci.guix.gnu.org"
-          # "https://bordeaux.guix.gnu.org"
-          # "https://guix.bordeaux.inria.fr"
-        ];
-      in
-      [
-        "--substitute-urls=${lib.concatStringsSep " " substituters}"
-        "-c"
-        "10"
+    extraArgs = let
+      substituters = [
+        "https://ci.guix.gnu.org"
+        # "https://bordeaux.guix.gnu.org"
+        # "https://guix.bordeaux.inria.fr"
       ];
+    in [
+      "--substitute-urls=${lib.concatStringsSep " " substituters}"
+      "-c"
+      "10"
+    ];
   };
 
-  environment.extraInit = ''
-    export GUILE_LOAD_PATH="$GUILE_LOAD_PATH:$HOME/.config/guix/current/share/guile/site/3.0"
+  environment.extraInit =
+    # bash
+    ''
+      export GUILE_LOAD_PATH="$GUILE_LOAD_PATH:$HOME/.config/guix/current/share/guile/site/3.0"
 
-    export GUILE_LOAD_COMPILED_PATH="$GUILE_LOAD_COMPILED_PATH:$HOME/.config/guix/current/share/guile/site/3.0"
-    export GUILE_LOAD_COMPILED_PATH="$GUILE_LOAD_COMPILED_PATH:$HOME/.config/guix/current/lib/guile/3.0/site-ccache"
-  '';
+      export GUILE_LOAD_COMPILED_PATH="$GUILE_LOAD_COMPILED_PATH:$HOME/.config/guix/current/share/guile/site/3.0"
+      export GUILE_LOAD_COMPILED_PATH="$GUILE_LOAD_COMPILED_PATH:$HOME/.config/guix/current/lib/guile/3.0/site-ccache"
+    '';
 
   # guix apps can't find these
   environment.sessionVariables = {
@@ -40,12 +40,19 @@
   };
 
   systemd.services."guix-substituters" = rec {
-    path = [ config.services.guix.package ];
+    path = [config.services.guix.package];
     script = ''
       set -x
       guix archive --authorize < ${./guix.bordeaux.inria.fr.pub}
     '';
-    wantedBy = [ "guix-daemon.service" ];
+    wantedBy = ["guix-daemon.service"];
     after = wantedBy;
+  };
+
+  systemd.services."guix-daemon" = {
+    environment = {
+      # GUIX_LOCPATH = lib.mkForce "/run/current-system/sw/lib/locale";
+      # GUIX_LOCPATH = lib.mkForce "/gnu/store/03v1svhv6wj9pd6awpdi5zn4wd31b23f-glibc-locales-2.35/lib/locale";
+    };
   };
 }
--- packages/guix/default.nix
+++ packages/guix/default.nix
@@ -2,29 +2,34 @@
   guix,
   symlinkJoin,
   writeShellScriptBin,
-
   # passed by nixos module
   storeDir ? null,
   stateDir ? null,
-}:
-
-let
-  profile = "/var/guix/profiles/per-user/ayats/current-guix";
+}: let
+  profiles = "/var/guix/profiles/per-user/ayats";
+  current-guix = "${profiles}/current-guix";
+  guix-home = "${profiles}/guix-home/profile";
   mkDispatch = bin:
     writeShellScriptBin bin ''
-      if [[ -f "${profile}/bin/${bin}" ]]; then
-        exec -a "$0" "${profile}/bin/${bin}" "$@"
+      if [[ -d "${guix-home}/lib/locale" ]]; then
+        >&2 echo "Found locales in user's guix home"
+        export GUIX_LOCPATH="${guix-home}/lib/locale:$GUIX_LOCPATH"
+      else
+        >&2 echo "Didn't find locales in user's guix home"
+      fi
+
+      if [[ -f "${current-guix}/bin/${bin}" ]]; then
+        exec -a "$0" "${current-guix}/bin/${bin}" "$@"
       else
         exec -a "$0" ${guix}/bin/${bin} "$@"
       fi
     '';
 in
-symlinkJoin {
-  inherit (guix) name pname version meta;
-  paths = [
-    (mkDispatch "guix")
-    (mkDispatch "guix-daemon")
-    guix
-  ];
-}
-
+  symlinkJoin {
+    inherit (guix) name pname version meta;
+    paths = [
+      (mkDispatch "guix")
+      (mkDispatch "guix-daemon")
+      guix
+    ];
+  }
