cadc59025d109bc2202a1ec54c06918b0098187a
Fernando Ayats <ayatsfer@gmail.com>
2024-08-04 14:21:27 +0200

    tweak cmp

--- modules/wrapper-manager/neovim/init.lua
+++ modules/wrapper-manager/neovim/init.lua
@@ -80,9 +80,8 @@ cmp.setup {
   mapping = cmp.mapping.preset.insert {
     -- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    -- ["<C-Space>"] = cmp.mapping.complete(),
-    -- ["<C-e>"] = cmp.mapping.abort(),
-    -- ["<CR>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<CR>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
