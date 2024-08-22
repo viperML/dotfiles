801716b0c33648811161a71086ddf66cf341dc5c
Fernando Ayats <fernando.ayats-llamas@inria.fr>
2024-08-22 11:58:19 +0200

    remove TS commands

--- modules/wrapper-manager/neovim/viper-init-plugin/lua/viper/tree-sitter.lua
+++ modules/wrapper-manager/neovim/viper-init-plugin/lua/viper/tree-sitter.lua
@@ -15,6 +15,14 @@ vim.list_extend(require("viper.lazy.specs"), {
       vim.wo.foldmethod = "expr"
       vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
       vim.opt.foldlevel = 99
+
+      -- Don't acidentally run these
+      vim.api.nvim_del_user_command("TSUpdate")
+      vim.api.nvim_del_user_command("TSUpdateSync")
+      vim.api.nvim_del_user_command("TSInstall")
+      vim.api.nvim_del_user_command("TSInstallSync")
+      vim.api.nvim_del_user_command("TSInstallFromGrammar")
+      vim.api.nvim_del_user_command("TSUninstall")
     end,
   },
 })
