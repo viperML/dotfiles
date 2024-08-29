b7cba2451ae47e533ff1f30067f60d9cf3e0316b
Fernando Ayats <ayatsfer@gmail.com>
2024-08-29 16:26:55 +0200

    markdown wrap

--- modules/wrapper-manager/neovim/viper-init-plugin/lua/viper/visual.lua
+++ modules/wrapper-manager/neovim/viper-init-plugin/lua/viper/visual.lua
@@ -227,3 +227,10 @@ for key, value in pairs(colorcolumns) do
     end,
   })
 end
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function ()
+    vim.opt_local.textwidth = 80
+  end
+})
