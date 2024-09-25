e9bdbb4a5dab0a1b749a7b5c0577e0f63d764b82
Fernando Ayats <ayatsfer@gmail.com>
2024-09-25 08:26:48 +0200

    tweak neovim

--- modules/wrapper-manager/neovim/viper-init-plugin/lua/viper/lsp_config.lua
+++ modules/wrapper-manager/neovim/viper-init-plugin/lua/viper/lsp_config.lua
@@ -5,6 +5,11 @@ vim.filetype.add {
   filename = { [".envrc"] = "bash" },
 }
 
+-- don't show parse errors in a separate window
+vim.g.zig_fmt_parse_errors = 0
+-- disable format-on-save from `ziglang/zig.vim`
+vim.g.zig_fmt_autosave = 0
+
 return {
   ["rnix"] = {
     cmd = { "nil" },
