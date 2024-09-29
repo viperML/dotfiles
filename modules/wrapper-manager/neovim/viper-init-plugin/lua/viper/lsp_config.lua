9972eae55c7943325bffbb28a255fc8c2df7ebba
Fernando Ayats <ayatsfer@gmail.com>
2024-09-29 17:05:13 +0200

    tweak yamlls

--- modules/wrapper-manager/neovim/viper-init-plugin/lua/viper/lsp_config.lua
+++ modules/wrapper-manager/neovim/viper-init-plugin/lua/viper/lsp_config.lua
@@ -83,6 +83,7 @@ return {
         },
         schemas = {
           ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = ".gitlab-ci.{yaml,yml}",
+          ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
         },
       },
     },
