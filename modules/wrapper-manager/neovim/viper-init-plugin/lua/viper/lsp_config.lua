d60fdcd5847397a5210f7ce8860b4f7f0f30e531
Fernando Ayats <ayatsfer@gmail.com>
2024-10-18 11:27:27 +0200

    tweak yaml

--- modules/wrapper-manager/neovim/viper-init-plugin/lua/viper/lsp_config.lua
+++ modules/wrapper-manager/neovim/viper-init-plugin/lua/viper/lsp_config.lua
@@ -85,6 +85,7 @@ return {
           ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = ".gitlab-ci.{yaml,yml}",
           ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
           ["https://raw.githubusercontent.com/canonical/cloud-init/main/cloudinit/config/schemas/versions.schema.cloud-config.json"] = "user-data.{yaml,yml}",
+          ["https://json.schemastore.org/hugo.json"] = "hugo.{yaml,yml}",
         },
       },
     },
