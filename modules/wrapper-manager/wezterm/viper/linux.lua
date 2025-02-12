d39018c17671031006fa65106e3f4c67e5982505
Fernando Ayats <ayatsfer@gmail.com>
2025-02-12 10:15:34 +0100

    tweak wezterm

--- modules/wrapper-manager/wezterm/viper/linux.lua
+++ modules/wrapper-manager/wezterm/viper/linux.lua
@@ -2,6 +2,8 @@ local wezterm = require("wezterm")
 
 local M = {}
 
+local os = require("os")
+
 M.apply_to_config = function(config)
   config.font = wezterm.font_with_fallback {
     { family = "iosevka-normal", weight = "Medium" },
@@ -10,10 +12,12 @@ M.apply_to_config = function(config)
   config.font_size = 12
 
   -- Fix for https://github.com/wez/wezterm/issues/5990
-  -- config.enable_wayland = true
-
-  config.enable_wayland = false
-  config.front_end = "WebGpu"
+  if os.getenv("NIXOS_OZONE_WL") ~= nil then
+    config.enable_wayland = true
+    config.front_end = "WebGpu"
+  else
+    config.enable_wayland = false
+  end
 
   config.xcursor_theme = "Adwaita"
 end
