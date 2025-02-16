b96aaeedc59fb2880f31e0f4524425fb2d3eec66
Fernando Ayats <ayatsfer@gmail.com>
2025-02-16 22:53:06 +0100

    tweak alacritty

--- modules/wrapper-manager/alacritty/default.nix
+++ modules/wrapper-manager/alacritty/default.nix
@@ -1,9 +1,27 @@
-{pkgs, ...}: {
+{ pkgs, lib, ... }:
+let
+  recursiveUpdateFold = builtins.foldl' lib.recursiveUpdate { };
+
+  config = recursiveUpdateFold [
+    (lib.importTOML ./alacritty.toml)
+    (lib.importTOML ./theme.toml)
+    {
+      terminal.shell.program = pkgs.writeShellScript "alacritty-start" ''
+        if [[ $(type -P zellij) ]]; then
+          exec zellij
+        else
+          exec "$SHELL"
+        fi
+      '';
+    }
+  ];
+in
+{
   wrappers.alacritty = {
     basePackage = pkgs.alacritty;
     flags = [
       "--config-file"
-      ./alacritty.toml
+      ((pkgs.formats.toml {}).generate "alacritty.toml" config)
     ];
   };
 }
