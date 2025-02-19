fe613ea29290b3ff9c6eab454417b6890f9bf959
Fernando Ayats <ayatsfer@gmail.com>
2025-02-19 12:19:50 +0100

    No WM for emacs

---
+++ packages/emacs/default.nix
@@ -0,0 +1,34 @@
+{
+  symlinkJoin,
+  emacsWithPackagesFromUsePackage,
+  emacs-pgtk,
+  makeWrapper,
+}:
+let
+  pkg = emacsWithPackagesFromUsePackage {
+    config = ./init.el;
+    package = emacs-pgtk;
+    alwaysEnsure = true;
+  };
+in
+symlinkJoin {
+  inherit (pkg)
+    name
+    meta
+    passthru
+    ;
+
+  paths = [ pkg ];
+  nativeBuildInputs = [ makeWrapper ];
+  postBuild = ''
+    for file in $out/bin/emacs $out/bin/emacs-*; do
+      wrapProgram $file \
+        --add-flags -nv
+    done
+
+    rm $out/share/applications
+    mkdir -p $out/share/applications
+    cp ${pkg}/share/applications/emacs.desktop $out/share/applications
+    sed -i -E "s#Exec=emacs([[:space:]]*)#Exec=${pkg}/bin/emacs\1#g" $out/share/applications/emacs.desktop
+  '';
+}
