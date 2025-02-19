cc456b9afcf4ab92851dd9f1940ebd960921fd4a
Fernando Ayats <ayatsfer@gmail.com>
2025-02-19 17:23:52 +0100

    fix wezterm

--- modules/wrapper-manager/wezterm/default.nix
+++ modules/wrapper-manager/wezterm/default.nix
@@ -1,39 +1,41 @@
 {
   pkgs,
-  inputs',
   ...
-}: {
-  wrappers.wezterm = let
-    smart-splits = (pkgs.callPackages ../neovim/generated.nix {}).smart-splits-nvim.src;
-  in {
-    basePackage = pkgs.wezterm;
-    # basePackage = inputs'.wezterm.packages.default;
-    env.WEZTERM_CONFIG_FILE.value =
-      pkgs.writeText "wezterm.lua"
-      # lua
-      ''
-        local wezterm = require("wezterm")
-        local config = wezterm.config_builder()
+}:
+{
+  wrappers.wezterm =
+    let
+      smart-splits = (pkgs.callPackages ../neovim/_sources/generated.nix { }).smart-splits-nvim.src;
+    in
+    {
+      basePackage = pkgs.wezterm;
+      # basePackage = inputs'.wezterm.packages.default;
+      env.WEZTERM_CONFIG_FILE.value =
+        pkgs.writeText "wezterm.lua"
+          # lua
+          ''
+            local wezterm = require("wezterm")
+            local config = wezterm.config_builder()
 
-        dofile("${./viper/init.lua}").apply_to_config(config)
-        dofile("${./viper/linux.lua}").apply_to_config(config)
+            dofile("${./viper/init.lua}").apply_to_config(config)
+            dofile("${./viper/linux.lua}").apply_to_config(config)
 
-        dofile("${smart-splits}/plugin/init.lua").apply_to_config(config, {
-          direction_keys = { "LeftArrow", "DownArrow", "UpArrow", "RightArrow" },
-          modifiers = {
-            move = "CTRL",
-            resize = "SHIFT|CTRL",
-          },
-        })
+            dofile("${smart-splits}/plugin/init.lua").apply_to_config(config, {
+              direction_keys = { "LeftArrow", "DownArrow", "UpArrow", "RightArrow" },
+              modifiers = {
+                move = "CTRL",
+                resize = "SHIFT|CTRL",
+              },
+            })
 
-        return config
-      '';
+            return config
+          '';
 
-    env.WEZTERM_CONFIG_FILE.force = true;
+      env.WEZTERM_CONFIG_FILE.force = true;
 
-    env.WEZTERM_LOG.value = builtins.concatStringsSep "," [
-      "window::os::wayland::window=off"
-      "info"
-    ];
-  };
+      env.WEZTERM_LOG.value = builtins.concatStringsSep "," [
+        "window::os::wayland::window=off"
+        "info"
+      ];
+    };
 }
