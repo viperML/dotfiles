679bfe884219b16f380707bf31fd4de8b45512f0
Fernando Ayats <ayatsfer@gmail.com>
2025-02-20 13:40:20 +0100

    No hyprlink rules

--- modules/wrapper-manager/wezterm/viper/init.lua
+++ modules/wrapper-manager/wezterm/viper/init.lua
@@ -217,44 +217,6 @@ M.apply_to_config = function(config)
   config.selection_word_boundary = " \t\n{}[]()\"'`«»‘’,="
   config.mouse_wheel_scrolls_tabs = false
   config.hide_mouse_cursor_when_typing = false
-  config.hyperlink_rules = {
-    -- Matches: a URL in parens: (URL)
-    -- Markdown: [text](URL title)
-    {
-      regex = "\\((\\w+://\\S+?)(?:\\s+.+)?\\)",
-      format = "$1",
-      highlight = 1,
-    },
-    -- Matches: a URL in brackets: [URL]
-    {
-      regex = "\\[(\\w+://\\S+?)\\]",
-      format = "$1",
-      highlight = 1,
-    },
-    -- Matches: a URL in curly braces: {URL}
-    {
-      regex = "\\{(\\w+://\\S+?)\\}",
-      format = "$1",
-      highlight = 1,
-    },
-    -- Matches: a URL in angle brackets: <URL>
-    {
-      regex = "<(\\w+://\\S+?)>",
-      format = "$1",
-      highlight = 1,
-    },
-    -- Then handle URLs not wrapped in brackets
-    -- regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
-    {
-      regex = "(?<![\\(\\{\\[<])\\b\\w+://\\S+",
-      format = "$0",
-    },
-    -- implicit mailto link
-    {
-      regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
-      format = "mailto:$0",
-    },
-  }
 
   apply_keybindings(config)
   apply_colorscheme(config)
