adbcfcfad1a67918c0b9f2065c20ab644a688b1e
Fernando Ayats <ayatsfer@gmail.com>
2025-03-14 14:08:50 +0100

    tweak tmux

--- modules/wrapper-manager/tmux/default.nix
+++ modules/wrapper-manager/tmux/default.nix
@@ -1,11 +1,19 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  plugins = with pkgs.tmuxPlugins; [
+  ];
+in
 {
   wrappers.tmux = {
     basePackage = pkgs.tmux;
     flags = [
       "-f"
       (pkgs.writeText "tmux-conf" ''
+        run-shell ${pkgs.tmuxPlugins.sensible.rtp}
+
         source-file ${./tmux.conf}
+
+        ${plugins |> (map (plugin: "run-shell ${plugin.rtp}")) |> (lib.concatStringsSep "\n")}
       '')
     ];
   };
