9d7655052217560da2b74a5b4cd47eebcd490679
Fernando Ayats <ayatsfer@gmail.com>
2025-05-05 15:02:01 +0200

    Add waybar

--- modules/maid/default.nix
+++ modules/maid/default.nix
@@ -1,25 +1,35 @@
-{ config, pkgs, ... }:
 {
-  file.xdg_config."hypr/hyprland.conf".source =
-    "{{home}}/Documents/dotfiles/modules/maid/hyprland.conf";
-
-  systemd.services = {
-    "waybar" = {
-      path = [ pkgs.waybar ];
-      script = ''
-        waybar
-      '';
-      wantedBy = [ "graphical-session.target" ];
+  config,
+  lib,
+  ...
+}:
+{
+  options = {
+    impureDotfilesPath = lib.mkOption {
+      type = lib.types.str;
+      default = "{{home}}/Documents/dotfiles";
     };
   };
 
-  systemd.targets.hyprland-session = {
-    unitConfig = {
-      Description = "hyprland compositor session";
-      BindsTo = ["graphical-session.target"];
-      Wants = ["graphical-session-pre.target" "xdg-desktop-autostart.target"];
-      After = ["graphical-session-pre.target"];
-      Before = ["xdg-desktop-autostart.target"];
+  imports = [
+    ./waybar
+  ];
+
+  config = {
+    file.xdg_config."hypr/hyprland.conf".source =
+      "${config.impureDotfilesPath}/modules/maid/hyprland/hyprland.conf";
+
+    systemd.targets.hyprland-session = {
+      unitConfig = {
+        Description = "hyprland compositor session";
+        BindsTo = [ "graphical-session.target" ];
+        Wants = [
+          "graphical-session-pre.target"
+          "xdg-desktop-autostart.target"
+        ];
+        After = [ "graphical-session-pre.target" ];
+        Before = [ "xdg-desktop-autostart.target" ];
+      };
     };
   };
 }
