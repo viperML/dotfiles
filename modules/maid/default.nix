8e4ffc90a72130b1d78f7e13b6db4b63738c3d2a
Fernando Ayats <ayatsfer@gmail.com>
2025-05-15 09:31:39 +0200

    Tweak HL

--- modules/maid/default.nix
+++ modules/maid/default.nix
@@ -22,6 +22,10 @@
     file.xdg_config."hypr/hyprland.conf".source =
       "${config.impureDotfilesPath}/modules/maid/hyprland/hyprland.conf";
 
+    systemd.tmpfiles.dynamicRules = [ 
+      "f {{xdg_config_home}}/hypr/monitors.conf 0644 {{user}} {{group}} - -"
+    ];
+
     systemd.targets.hyprland-session = {
       unitConfig = {
         Description = "hyprland compositor session";
