4149d176c7b8da615f6012f4ae0bedd2dfc3d244
Fernando Ayats <ayatsfer@gmail.com>
2025-05-20 08:41:30 +0200

    Change zorah

--- hosts/zorah/default.nix
+++ hosts/zorah/default.nix
@@ -12,22 +12,7 @@ import ../. {
     ../../modules/nixos/silent-boot.nix
 
     #-- Environment
-    # {services.displayManager.autoLogin.user = "ayats";}
-    ../../modules/nixos/hyprland.nix
-    {
-      specialisation.gnome = {
-        inheritParentConfig = true;
-        configuration = {
-          environment.etc."specialisation".text = "gnome";
-          disabledModules = [
-            ../../modules/nixos/hyprland.nix
-          ];
-          imports = [
-            ../../modules/nixos/gnome.nix
-          ];
-        };
-      };
-    }
+    ../../modules/nixos/gnome.nix
 
     #-- Other
     ../../modules/nixos/tailscale.nix
