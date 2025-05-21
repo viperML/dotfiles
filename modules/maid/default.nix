ef6f1d03a258259028b8444ea127a80d6886ca84
Fernando Ayats <ayatsfer@gmail.com>
2025-05-21 13:54:01 +0200

    Tweak gnome

--- modules/maid/default.nix
+++ modules/maid/default.nix
@@ -15,21 +15,28 @@
   config = {
     file.home.".gitconfig".source = pkgs.git-viper.gitconfig;
 
+    gsettings.settings = {
+      "org.gnome.mutter"."experimental-features" = [
+        "scale-monitor-framebuffer"
+        "xwayland-native-scaling"
+      ];
+    };
+
     dconf.settings = {
       "/org/gnome/desktop/interface/color-scheme" = "prefer-dark";
       "/org/gnome/desktop/interface/cursor-size" = 24;
       "/org/gnome/desktop/interface/cursor-theme" = "DMZ-White";
       "/org/gnome/desktop/interface/document-font-name" = "Roboto 11";
-      "/org/gnome/desktop/interface/enable-hot-corners" = "false";
+      "/org/gnome/desktop/interface/enable-hot-corners" = false;
       "/org/gnome/desktop/interface/font-name" = "Roboto 11";
-      "/org/gnome/desktop/interface/gtk-enable-primary-paste" = "false";
+      "/org/gnome/desktop/interface/gtk-enable-primary-paste" = false;
       "/org/gnome/desktop/interface/gtk-theme" = "adw-gtk3-dark";
       "/org/gnome/desktop/interface/icon-theme" = "Adwaita";
-      "/org/gnome/desktop/lockdown/disable-lock-screen" = "true";
+      "/org/gnome/desktop/lockdown/disable-lock-screen" = true;
       "/org/gnome/desktop/peripherals/mouse/accel-profile" = "flat";
       "/org/gnome/desktop/wm/preferences/titlebar-font" = "Roboto Bold 11";
       "/org/gnome/desktop/peripherals/touchpad/click-method" = "areas";
-      "/org/gnome/desktop/interface/show-battery-percentage" = "true";
+      "/org/gnome/desktop/interface/show-battery-percentage" = true;
       "/org/gnome/desktop/interface/clock-format" = "12h";
       "/org/gnome/desktop/wm/preferences/resize-with-right-button" = true;
       "/org/gnome/desktop/search-providers/disabled" = [
@@ -43,6 +50,22 @@
         "appindicatorsupport@rgcjonas.gmail.com"
         "dash-to-panel@jderose9.github.com"
       ];
+      "/org/gnome/shell/keybindings/switch-to-application-1" = [ ];
+      "/org/gnome/shell/keybindings/switch-to-application-2" = [ ];
+      "/org/gnome/shell/keybindings/switch-to-application-3" = [ ];
+      "/org/gnome/shell/keybindings/switch-to-application-4" = [ ];
+      "/org/gnome/desktop/wm/keybindings/switch-to-workspace-1" = [ "<Super>1" ];
+      "/org/gnome/desktop/wm/keybindings/switch-to-workspace-2" = [ "<Super>2" ];
+      "/org/gnome/desktop/wm/keybindings/switch-to-workspace-3" = [ "<Super>3" ];
+      "/org/gnome/desktop/wm/keybindings/switch-to-workspace-4" = [ "<Super>4" ];
+      "/org/gnome/desktop/wm/keybindings/move-to-workspace-1" = [ "<Shift><Super>1" ];
+      "/org/gnome/desktop/wm/keybindings/move-to-workspace-2" = [ "<Shift><Super>2" ];
+      "/org/gnome/desktop/wm/keybindings/move-to-workspace-3" = [ "<Shift><Super>3" ];
+      "/org/gnome/desktop/wm/keybindings/move-to-workspace-4" = [ "<Shift><Super>4" ];
+      "/org/gnome/desktop/wm/keybindings/close" = [ "<Super>q" ];
+      "/org/gnome/mutter/dynamic-workspaces" = false;
+      "/org/gnome/desktop/wm/preferences/num-workspaces" = 4;
+      "/org/gnome/mutter/workspaces-only-on-primary" = false;
     };
   };
 }
