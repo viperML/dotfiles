1de2d8368553534f10dbe54db302fba7350720aa
Fernando Ayats <ayatsfer@gmail.com>
2025-06-14 19:43:42 +0200

    Fix tpm scripts

--- modules/nixos/tpm2/git-key-command.sh
+++ modules/nixos/tpm2/git-key-command.sh
@@ -1,14 +1,20 @@
 #! /usr/bin/env bash
-# set -x
-# unset SSH_ASKPASS
+set -u
+unset SSH_ASKPASS
 
-lib=/run/current-system/sw/lib/libtpm2_pkcs11.so
+lib="/run/current-system/sw/lib/libtpm2_pkcs11.so"
 
-# if ! ssh-add -L | grep --quiet libtpm2_pkcs11.so; then
-#     set -e
-#     ssh-add -s $lib
-#     set +e
-# fi
+pub="$(ssh-keygen -D $lib)"
 
-echo "key::$(ssh-keygen -D $lib)"
+if ! ssh-add -L | grep -q "$pub"; then
+    echo "" | ssh-add -s "$lib" > /dev/null 2>&1
+    exit=$?
+
+    if [ $exit -ne 0 ]; then
+        echo "Failed to add TPM2 key to ssh-agent" >&2
+        exit 1
+    fi
+fi
+
+echo "key::$pub"
 
--- modules/nixos/tpm2/tpm-reconfigure.sh
+++ modules/nixos/tpm2/tpm-reconfigure.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 set -ux
 
-rm -vf ~/.local/share/tpm2-pkcs11/tpm2_pkcs11.sqlite3
+rm -rvf ~/.local/share/tpm2-pkcs11
+rm -rvf ~/.tpm2_pkcs11
 
 tpm2_ptool init
 
