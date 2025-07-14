06be642d451929bd2981dfbcca38318972091014
Fernando Ayats <ayatsfer@gmail.com>
2025-07-14 12:57:45 +0200

    Tweak ghostty

--- modules/wrapper-manager/ghostty/default.nix
+++ modules/wrapper-manager/ghostty/default.nix
@@ -1,8 +1,23 @@
-{pkgs, ...}: {
+{ pkgs, lib, ... }:
+let
+
+  config = pkgs.writeText "ghostty-config" (
+    [
+      ./config
+      ./catpuccin-mocha.conf
+    ]
+    |> (map builtins.readFile)
+    |> (lib.concatStringsSep "\n")
+  );
+in
+{
   wrappers.ghostty = {
     basePackage = pkgs.ghostty;
     prependFlags = [
-      "--config-file=${./config}"
+      "--config-file=${config}"
     ];
+    programs.ghostty-unwrapped = {
+      target = "ghostty";
+    };
   };
 }
