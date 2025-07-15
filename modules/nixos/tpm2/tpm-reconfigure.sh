bf8003030af0f166194e79eac193e39912e3438f
Fernando Ayats <ayatsfer@gmail.com>
2025-07-15 09:12:45 +0200

    Tweak tpm-reconfigure

--- modules/nixos/tpm2/tpm-reconfigure.sh
+++ modules/nixos/tpm2/tpm-reconfigure.sh
@@ -9,3 +9,5 @@ tpm2_ptool init
 tpm2_ptool addtoken --pid 1 --label ssh --userpin "" --sopin ""
 
 tpm2_ptool addkey --label ssh --userpin "" --algorithm ecc256 --key-label "$USER $(hostname) tpm2"
+
+ssh-keygen -D /run/current-system/sw/lib/libtpm2_pkcs11.so > ~/.ssh/tpm2.pub
