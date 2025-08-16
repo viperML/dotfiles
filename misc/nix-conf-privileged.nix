8292326ec8930ab90466056cce2ff73aa1a57f2d
Fernando Ayats <ayatsfer@gmail.com>
2025-08-16 21:02:41 +0200

    Remove substitutes

--- misc/nix-conf-privileged.nix
+++ misc/nix-conf-privileged.nix
@@ -1,14 +1,14 @@
 {
   extra-substituters = [
-    "https://nix-community.cachix.org"
-    "https://viperml.cachix.org"
+    # "https://nix-community.cachix.org"
+    # "https://viperml.cachix.org"
     # "https://nixpkgs-unfree.cachix.org"
     # "https://cuda-maintainers.cachix.org"
   ];
 
   extra-trusted-public-keys = [
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    "viperml.cachix.org-1:qZhKBMTfmcLL+OG6fj/hzsMEedgKvZVFRRAhq7j8Vh8="
+    # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    # "viperml.cachix.org-1:qZhKBMTfmcLL+OG6fj/hzsMEedgKvZVFRRAhq7j8Vh8="
     # "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
     # "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
   ];
