b44705775255aec4a0469a2a7e55e2e8e15eb5c0
Fernando Ayats <fayats@bsc.es>
2025-10-07 11:27:08 +0200

    Cleanup nixos call

--- hosts/default.nix
+++ hosts/default.nix
@@ -2,18 +2,13 @@ let
   sources = import ../npins;
   pkgs = import ../packages;
 in
-(
-  { modules }:
-  import "${sources.nixpkgs}/nixos/lib/eval-config.nix" {
-    system = null;
-    modules = [
-      {
-        config.nixpkgs.pkgs = pkgs;
-      }
-      ../modules/nixos/common-gui.nix
-      "${sources.nix-common}/nixos"
-      (import sources.lanzaboote).nixosModules.lanzaboote
-      (import sources.nix-maid).nixosModules.default
-    ] ++ modules;
-  }
+{ modules }:
+pkgs.nixos (
+  [
+    ../modules/nixos/common-gui.nix
+    "${sources.nix-common}/nixos"
+    (import sources.lanzaboote).nixosModules.lanzaboote
+    (import sources.nix-maid).nixosModules.default
+  ]
+  ++ modules
 )
