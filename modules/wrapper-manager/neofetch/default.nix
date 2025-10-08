350dc6e11504dc80f647aabdd21ac11389712a72
Fernando Ayats <fayats@bsc.es>
2025-10-08 08:42:38 +0200

    Fix neofetch

--- modules/wrapper-manager/neofetch/default.nix
+++ modules/wrapper-manager/neofetch/default.nix
@@ -1,6 +1,12 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   wrappers.neofetch = {
-    basePackage = pkgs.neofetch.override {x11Support = false;};
-    flags = ["--config" ./config.sh "--ascii" ./logo];
+    basePackage = pkgs.neofetch.override { x11Support = false; };
+    prependFlags = [
+      "--config"
+      ./config.sh
+      "--ascii"
+      ./logo
+    ];
   };
 }
