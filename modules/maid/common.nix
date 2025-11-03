43030c01349a8219688e8d168b2a5cb2a14b316b
Fernando Ayats <fayats@bsc.es>
2025-11-03 14:00:43 +0100

    Cleanup

--- modules/maid/common.nix
+++ modules/maid/common.nix
@@ -9,9 +9,6 @@
   ];
 
   file.home.".gitconfig".source = pkgs.git-viper.gitconfig;
-  systemd.tmpfiles.dynamicRules = [
-    "D {{home}}/.spack 0755 {{user}} {{group}} 0 -"
-  ];
 
   file.xdg_config."nixpkgs/config.nix".text = ''
     { allowUnfree = true; }
