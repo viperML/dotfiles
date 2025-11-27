bde2c534544124de8de806669b62ff5268a0dd0c
Fernando Ayats <fayats@bsc.es>
2025-11-27 11:30:56 +0100

    Update cajon

--- misc/trailing.sh
+++ misc/trailing.sh
@@ -8,7 +8,9 @@ echo ".vscode" >> "$dir/info/exclude"
 mkdir -v "$root/.vscode"
 tee "$root/.vscode/settings.json" <<EOF
 {
-    "files.trimTrailingWhitespace": false
+    "files.trimTrailingWhitespace": false,
+    "files.trimFinalNewlines": false,
+    "files.insertFinalNewline": false
 }
 EOF
 
