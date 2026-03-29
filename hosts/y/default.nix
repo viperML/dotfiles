7ba3a6401cf919353fe1c601ffcd9e2cee9c70bc
Fernando Ayats <fayats@bsc.es>
2026-03-29 21:35:50 +0200

    Cleanup y

--- hosts/y/default.nix
+++ hosts/y/default.nix
@@ -6,19 +6,14 @@ import ../. [
   ../../modules/nixos/tmpfs.nix
   ../../modules/nixos/tpm2
   ../../modules/nixos/users.nix
-  # ../../modules/nixos/yubikey
 
   #-- Environment
   { services.displayManager.autoLogin.user = "ayats"; }
-  # ../../modules/nixos/gnome.nix
   ../../modules/nixos/plasma6.nix
 
   #-- Other
   ../../modules/nixos/tailscale.nix
-  # ../../modules/nixos/printing.nix
   ../../modules/nixos/podman.nix
   ../../modules/nixos/silent-boot.nix
-  # ./gitlab-runner.nix
   ./sourcebot.nix
-  ../../modules/nixos/firefox
 ]
