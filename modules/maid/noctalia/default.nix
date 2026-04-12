2917066cb571edb2b9e46dc425a6e00d6666ed03
Fernando Ayats <ayatsfer@gmail.com>
2026-04-12 15:51:31 +0200

    Tweak hyprland

--- modules/maid/noctalia/default.nix
+++ modules/maid/noctalia/default.nix
@@ -1,23 +1,37 @@
 { pkgs, lib, ... }:
 let
   targets = [ "graphical-session.target" ];
+  sources = import ../../../npins;
+
+  mkLink = f: {
+    file.xdg_config."noctalia/${f}".source = builtins.toString ./${f};
+  };
 in
 {
-  packages = [
-    pkgs.noctalia-shell
+  config = lib.mkMerge [
+    {
+      packages = [
+        pkgs.noctalia-shell
+      ];
+
+      systemd.services."noctalia" = {
+        script = ''
+          set -a
+          eval "$(systemctl --user show-environment)"
+          export PATH="${lib.makeBinPath [ pkgs.networkmanagerapplet ]}:$PATH"
+          exec ${lib.getExe pkgs.noctalia-shell}
+        '';
+
+        partOf = targets;
+        after = targets;
+        wantedBy = targets;
+      };
+
+      file.xdg_config."noctalia/plugins/network-manager-vpn".source =
+        "${sources.noctalia-plugins}/network-manager-vpn";
+    }
+    (mkLink "colors.json")
+    (mkLink "plugins.json")
+    (mkLink "settings.json")
   ];
-
-  file.xdg_config."noctalia".source = builtins.toString ./.;
-
-  systemd.services."noctalia" = {
-    script = ''
-      set -a
-      eval "$(systemctl --user show-environment)"
-      ${lib.getExe pkgs.noctalia-shell}
-    '';
-
-    partOf = targets;
-    after = targets;
-    wantedBy = targets;
-  };
 }
