42a699069ccfa2316f13d980229c6a806b14ad65
Fernando Ayats <ayatsfer@gmail.com>
2026-05-03 22:11:37 +0200

    Tweak niri

--- modules/nixos/niri.nix
+++ modules/nixos/niri.nix
@@ -10,4 +10,14 @@
   programs.niri = {
     enable = true;
   };
+
+  programs.uwsm.waylandCompositors = {
+    niri = {
+      prettyName = "Niri";
+      comment = "Hyprland compositor managed by UWSM";
+      binPath = "/run/current-system/sw/bin/niri-session";
+    };
+  };
+
+  services.displayManager.defaultSession = "niri-uwsm";
 }
